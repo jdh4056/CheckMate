228a8db88fe240f7b76d6c39c3bc289fb67b3a1b
jdh4056 <jdh4056@naver.com>
2025-05-25 12:27:19 +0900

    merge conflit 해결(db 변경이슈) & 테스트 포함

--- src/main/java/goldstamp/two/domain/Disease.java
+++ src/main/java/goldstamp/two/domain/Disease.java
@@ -10,7 +10,8 @@ import lombok.Setter;
 @Setter
 public class Disease {
 
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "disease_id")
     private long id;
 
@@ -21,11 +22,11 @@ public class Disease {
     @Column(length = 1000)
     private String name;
 
-<<<<<<< HEAD
-    @Column(name = "description")
-    private String explain; //MySQL에서 예약어라서 컬럼명으로 쓰면 안 됨
-=======
-    @Column(length = 3000)
+
+    //@Column(name = "description")
+    //private String explain; //MySQL에서 예약어라서 컬럼명으로 쓰면 안 됨
+
+    @Column(length = 3000, name = "description")
     private String explain;
->>>>>>> upstream/main
 }
+
