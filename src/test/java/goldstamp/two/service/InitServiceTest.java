785551546219bdbc2d481c590d4759ca26faf9bc
jdh4056 <jdh4056@naver.com>
2025-05-26 21:07:21 +0900

    병합 테스트 initServiceTest수정

--- src/test/java/goldstamp/two/service/InitServiceTest.java
+++ src/test/java/goldstamp/two/service/InitServiceTest.java
@@ -1,10 +1,12 @@
 package goldstamp.two.service;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Transactional
 public class InitServiceTest {
 
     @Autowired
