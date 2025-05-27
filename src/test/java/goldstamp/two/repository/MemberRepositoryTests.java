aa0de7746821dec1175eed3e99bb6c3017d093be
jdh4056 <jdh4056@naver.com>
2025-05-27 12:05:47 +0900

    memberRepositoryTests 수정 배포 테스트

--- src/test/java/goldstamp/two/repository/MemberRepositoryTests.java
+++ src/test/java/goldstamp/two/repository/MemberRepositoryTests.java
@@ -42,7 +42,39 @@ public class MemberRepositoryTests {
 
             memberRepository.save(member);
         }
+        String email = "user9@aaa.com";
+
+        Member member = memberRepository.getWithRoles(email); //자동으로 조인처리가 되었는지 확인
+
+        log.info("-----------------");
+        log.info(member);
+        log.info(member.getMemberRoleList());
     }
+
+    /*
+    @Test
+    public void testInsertMember() {
+        for (int i = 0; i < 10 ; i++) {
+
+            Member member = Member.builder()
+                    .loginId("user"+i+"@aaa.com")
+                    .password( passwordEncoder.encode("1111"))
+                    .name("USER"+i)
+                    .build();
+            member.addRole(MemberRole.USER);
+
+            if(i > 5) {
+                member.addRole(MemberRole.MANAGER);
+            }
+            if(i >= 8){
+                member.addRole(MemberRole.ADMIN);
+            }
+
+            memberRepository.save(member);
+        }
+    }
+
+     */
 /*
     @Test
     public void testRead() {
